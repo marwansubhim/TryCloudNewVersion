ca4e9984941828452a8989a2d4524c3a1cfbf815
Marwan Subhi <marwan.subhi4@gmail.com>
2021-12-25 17:27:11 -0500

    Adjusted the driver utility returns to be synchronized

--- src/test/java/com/tryCloud/pages/LoginPage.java
+++ src/test/java/com/tryCloud/pages/LoginPage.java
@@ -2,14 +2,16 @@ package com.tryCloud.pages;
 
 import org.openqa.selenium.By;
 
+
+import java.util.List;
+
 /**
  * Created by: Marwan on 12/25/2021 around 12:15 PM
  */
-public class LoginPage {
+public class LoginPage  {
 
     public By userInputBox = By.id("user");
     public By passwordBox = By.id("password");
     public By loginButton = By.id("submit-form");
-
 
 }
